8caa25580a95aaf25ca58935c578bfd9f1eacb02
chhavisujeebun <chhavisujeebun@cmail.carleton.ca>
2020-10-09 21:59:49 +0400

    uploading Lab-4

---
+++ lab-4/lab4.py
@@ -0,0 +1,17 @@
+
+from urllib.parse import urlencode 
+import time
+import http.client as http
+from urllib.request import urlopen
+
+key = "KX7M32W7JZD2UCO0"
+ProjectGroup= "L1-F-1"
+Identifier="b"
+cmailaddress="chhavisujeebun@cmail.carleton.ca"
+
+URL='http://api.thingspeak.com/update?api_key='
+HEADER='&field1={}&field2{}&field3{}'.format(ProjectGroup,Identifier,cmailaddress)
+new_url=URL+key+HEADER
+print(new_url)
+update= urlopen(new_url)
+print(update)
